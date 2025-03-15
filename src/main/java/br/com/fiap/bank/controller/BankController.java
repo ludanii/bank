626176e712134ea8506e6eeef93a7415abbd2d15
ludanii <ludanielle2005@hotmail.com>
2025-03-14 22:24:27 -0300

    Adicionando o cadastro dos usuários.

--- src/main/java/br/com/fiap/bank/controller/BankController.java
+++ src/main/java/br/com/fiap/bank/controller/BankController.java
@@ -1,21 +1,65 @@
 package br.com.fiap.bank.controller;
 
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale.Category;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.com.fiap.bank.model.Tipo;
+import br.com.fiap.bank.model.User;
 
 @RestController
 public class BankController {
     private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private List<User> repository = new ArrayList<>();
+    
+    private void validarCampos(User user) {
+        if (user.getNomeTitular().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Categoria não encontrada!");
+        }
+        if (user.getCpf().isEmpty()) {
+            throw  new ResponseStatusException(HttpStatus.BAD_REQUEST,"CPF é obrigatório!");
+        }
+        if (user.getDataAbertura().isAfter(LocalDate.now())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"A data de abertura não pode ser no futuro!");
+        }
+        if (user.getSaldoInicial() < 0){
+            throw  new ResponseStatusException(HttpStatus.BAD_REQUEST,"O saldo inicial deve ser positivo!");
+        }
+        if (!Arrays.asList(Tipo.values()).contains(user.getTipo())){
+            throw  new ResponseStatusException(HttpStatus.BAD_REQUEST,"O tipo inválido! (Corrente, Poupança ou Salário)");
+        }
+    }
 
     @GetMapping("/")
     public ResponseEntity<String> getNomes() {
         log.info("Buscando Informações do projeto...");
         var infoProjeto = "Nome do projeto: Bank \nNomes: Luisa Danielle e Michelle Pontenza";
         return ResponseEntity.ok(infoProjeto);
+    }
+    
+
+    @PostMapping("/bank")
+    public ResponseEntity<Object> cadastro(@RequestBody User user){
+        validarCampos(user);
+        log.info("Cadastrando " + user);
+        repository.add(user);
+        return ResponseEntity.ok(user);
+        
     }
     
 }
--- src/main/java/br/com/fiap/bank/model/User.java
+++ src/main/java/br/com/fiap/bank/model/User.java
@@ -1,10 +1,94 @@
 package br.com.fiap.bank.model;
 
 import java.time.LocalDate;
+import java.util.Random;
 
 public class User {
-    Long numero, saldoInicial;
-    int agencia;
-    String nomeTitular;
-    LocalDate dataAbertura;
+    private Long numero;
+    private double saldoInicial;
+    private int agencia;
+    private String nomeTitular, status, cpf;
+    private LocalDate dataAbertura;
+    private Tipo tipo;
+
+    public User(Long numero, double saldoInicial, int agencia, String cpf, String nomeTitular, LocalDate dataAbertura, String status, Tipo tipo) {
+        this.numero = Math.abs(new Random().nextLong());
+        this.saldoInicial = saldoInicial;
+        this.agencia = agencia;
+        this.cpf = cpf;
+        this.nomeTitular = nomeTitular;
+        this.dataAbertura = dataAbertura;
+        this.status = status;
+        this.tipo = tipo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldoInicial() {
+        return saldoInicial;
+    }
+
+    public void setSaldoInicial(double saldoInicial) {
+        this.saldoInicial = saldoInicial;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public LocalDate getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDate dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String toString() {
+        return agencia + " " + nomeTitular + " " + tipo + " - " + saldoInicial;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+    
 }
+
